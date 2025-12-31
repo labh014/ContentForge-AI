5a3240cbbdd934358d3a2692d09a0af95e689e72
labh014 <labhverma551@gmail.com>
2025-12-31 14:31:06 +0530

    add deployed backend api

--- automation/config/env.js
+++ automation/config/env.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 module.exports = {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    BACKEND_API: "http://localhost:3000/api/articles"
+    BACKEND_API: process.env.BACKEND_API || "http://localhost:3000/api/articles"
 };
